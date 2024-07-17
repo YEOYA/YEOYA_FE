dc3c3e8c8ef208d0178568934563308076a5628e
six-standard <dbrrl1127@gmail.com>
2024-07-17 09:33:34 +0900

    add: input 컴포넌트 추가

---
+++ src/components/Input.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { default as Opened } from "../assets/Opened.svg?react";
+import { default as Closed } from "../assets/Closed.svg?react";
+
+export const Input = ({
+  label,
+  value,
+  onChange,
+  id = "",
+  placeholder,
+  secure,
+}) => {
+  const [visible, setVisible] = useState(false);
+
+  const clickVisible = () => setVisible((prev) => !prev);
+
+  return (
+    <div>
+      <span className="text-[16px] font-bold text-black">{label}</span>
+      <div className="relative flex items-center w-full h-fit">
+        <input
+          type={(!!secure && visible) || !!!secure ? "text" : "password"}
+          placeholder={placeholder}
+          id={id}
+          className={`w-full h-10 outline-none border-b-2 text-medium20 ${
+            !!value ? "border-black" : "border-gray300"
+          }`}
+          value={value}
+          onChange={onChange}
+        />
+        {!!secure && (
+          <div className="absolute right-5 cursor-pointer">
+            {visible ? (
+              <Opened onClick={clickVisible} />
+            ) : (
+              <Closed onClick={clickVisible} />
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};

4253485dd77b229c83e6fe05161a5e59ab9a7812
six-standard <dbrrl1127@gmail.com>
2024-07-17 14:56:16 +0900

    fix: 레이이웃 수정

--- src/router/Layout.jsx
+++ src/router/Layout.jsx
@@ -2,8 +2,8 @@ import { Outlet } from "react-router-dom";
 
 export const Layout = () => {
   return (
-    <div className="flex justify-center w-full h-[100vh] bg-gray-500">
-      <div className="max-w-screen-md w-full h-full bg-[#ffffff]">
+    <div className="flex justify-center w-full h-[100vh] bg-gray-500 overflow-y-auto">
+      <div className="max-w-screen-md w-full h-fit bg-[#ffffff]">
         <Outlet />
       </div>
     </div>
